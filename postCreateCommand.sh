a67d5a37433b26871cd54e222334d516d524fec9
Dan Erickson <dan@danerick.com>
2024-09-13 16:23:41 -0400

    Automatically create python virtual environment

--- postCreateCommand.sh
+++ postCreateCommand.sh
@@ -2,3 +2,10 @@
 # You can put any custom commands in here to modify your environment.
 # The script is ran from your workspace directory within the container.
 # This means you can execute scripts in your codebase in the container.
+
+# Verify there is a python virtual environment, and add it to the bash config
+# file so it automatically activates in new terminals
+if [ ! -f env/pyvenv.cfg ]; then
+    python -m venv env
+fi
+echo "source env/bin/activate" >> /home/vscode/.bashrc
